9a3ff6559c1b47542861374afa062f54e34ed1b9
Noyan Alimov <noyan@Noyans-MacBook-Pro.local>
2020-07-30 02:22:19 +0100

    solved problem 2

---
+++ problem_2/explanation_2.md
@@ -0,0 +1,2 @@
+In this problem we used a list data structure to add data and return it at the end.
+Our function runs in O(n^2) time. It uses a for loop and recursion inside a for loop.
---
+++ problem_2/problem_2.py
@@ -0,0 +1,38 @@
+import os
+
+
+def find_files(suffix, path):
+    paths = []
+
+    if os.path.isfile(path):
+        if path.endswith(suffix):
+            paths.append(path)
+    else:
+        if os.path.isdir(path):
+            sub_directories = os.listdir(path)
+            for sub_directory in sub_directories:
+                sub_paths = find_files(suffix, f'{path}/{sub_directory}')
+                for sub_path in sub_paths:
+                    paths.append(sub_path)
+    
+    return paths
+
+
+def test_function_1(suffix, path):
+    paths = find_files(suffix, path)
+    if paths and type(paths) == list:
+        print('Test Success')
+    else:
+        print('Test Failed')
+
+
+def test_function_2(suffix, path):
+    paths = find_files(suffix, path)
+    if not paths:
+        print('Test Success')
+    else:
+        print('Test Failed') 
+
+test_function_1('.c', f'{os.path.abspath(os.getcwd())}/testdir')  # expect to return a non-empty list 
+test_function_2('', '')  # expect to return an empty list
+test_function_2('jfhdalksjdfh', 'sjdfhsakajfhk')  # expect to return an empty lest
