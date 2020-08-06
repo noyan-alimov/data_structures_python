d55366ecad170fa9be0d5b7f67c289cff8c2a69b
Noyan Alimov <noyan.alimov@gmail.com>
2020-08-06 15:43:25 +0100

    finished intersection function and explanation

--- problem_6/problem_6.py
+++ problem_6/problem_6.py
@@ -90,22 +90,32 @@ for i in element_1:
 for i in element_2:
     linked_list_2.append(i)
 
-# print (union(linked_list_1,linked_list_2))
-print (intersection(linked_list_1,linked_list_2))
+print (union(linked_list_1,linked_list_2)) # expect a linked list that has values of the first and second linked list
+print (intersection(linked_list_1,linked_list_2)) # expect a linked list that has values only that are in both first and secont linked lists
 
-# # Test case 2
 
-# linked_list_3 = LinkedList()
-# linked_list_4 = LinkedList()
+# Test case 2
 
-# element_1 = [3,2,4,35,6,65,6,4,3,23]
-# element_2 = [1,7,8,9,11,21,1]
+linked_list_3 = LinkedList()
+linked_list_4 = LinkedList()
 
-# for i in element_1:
-#     linked_list_3.append(i)
+element_1 = [3,2,4,35,6,65,6,4,3,23]
+element_2 = [1,7,8,9,11,21,1]
 
-# for i in element_2:
-#     linked_list_4.append(i)
+for i in element_1:
+    linked_list_3.append(i)
 
-# print (union(linked_list_3,linked_list_4))
-# print (intersection(linked_list_3,linked_list_4))
+for i in element_2:
+    linked_list_4.append(i)
+
+print (union(linked_list_3,linked_list_4)) # expect a linked list that has values of the first and second linked list
+print (intersection(linked_list_3,linked_list_4)) # expect an empty linked list because there are no common values between first and second linked list
+
+
+# Test case 3
+
+linked_list_5 = LinkedList()
+linked_list_6 = LinkedList()
+
+print (union(linked_list_5,linked_list_6)) # expect an empty linked list
+print (intersection(linked_list_5,linked_list_6)) # expect an empty linked list
